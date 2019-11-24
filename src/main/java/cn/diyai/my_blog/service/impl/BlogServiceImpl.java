c376bb878bc016a907205305cb80ddf174151f29
wangxiaomin <wangxiaomin@800617.com>
2019-11-24 21:50:48 +0800

    重构

--- src/main/java/cn/diyai/my_blog/service/impl/BlogServiceImpl.java
+++ src/main/java/cn/diyai/my_blog/service/impl/BlogServiceImpl.java
@@ -11,9 +11,6 @@ import java.util.Optional;
 
 /**
  * Blog 服务.
- *
- * @author <a href="https://waylau.com">Way Lau</a>
- * @since 1.0.0 2017年4月7日
  */
 @Service
 public class BlogServiceImpl implements BlogService {
